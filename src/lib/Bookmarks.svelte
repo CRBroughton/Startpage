d454a08b5c422feeb1b05b395d923fc7494e3362
CRBroughton <crbroughton@posteo.uk>
2023-04-10 16:30:14 +0100

    feat: :sparkles: categories are now sorted

--- src/lib/Bookmarks.svelte
+++ src/lib/Bookmarks.svelte
@@ -16,22 +16,35 @@
   }
 
   let bookmarks: Bookmark[] = []
+  let categories: string[] = []
 
   const handleResponse = async () => {
     const response = await pb.collection('bookmarks').getFullList<Bookmark>()
 
     bookmarks = response
+    bookmarks = response.sort((a, b) => a.value.localeCompare(b.value))
+
+    response.forEach((bookmark) => {
+      if (categories.includes(bookmark.category)) return
+      categories = [...categories, bookmark.category]
+    })
   }
+
+  const filteredCategory = (category: string) =>
+    bookmarks.filter((bookmark) => bookmark.category === category)
 
   onMount(async () => handleResponse())
 </script>
 
 <div class="flex flex-col gap-2">
-  {#each bookmarks as bookmark}
-    <Card href={bookmark.url}>
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        {bookmark.value}
-      </h5>
-    </Card>
+  {#each categories.sort() as category}
+    <p>{category}</p>
+    {#each filteredCategory(category) as bookmark}
+      <Card href={bookmark.url}>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {bookmark.value}
+        </h5>
+      </Card>
+    {/each}
   {/each}
 </div>
